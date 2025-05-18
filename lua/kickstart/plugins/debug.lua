19f6200f68595583149a4be0b7f96a4fdc238158
João Holanda <joao.holanda@gmail.com>
2025-05-18 15:04:29 -0300

    primeira versão funcional para fortran

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -136,6 +136,60 @@ return {
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
+    dap.adapters.gdb = {
+      id = 'gdb',
+      type = 'executable',
+      command = 'gdb',
+      args = { '--quiet', '--interpreter=dap' },
+    }
+
+    dap.configurations.fortran = {
+      {
+        name = 'Run executable (GDB)',
+        type = 'gdb',
+        request = 'launch',
+        -- This requires special handling of 'run_last', see
+        -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+        program = function()
+          local path = vim.fn.input {
+            prompt = 'Path to executable: ',
+            default = vim.fn.getcwd() .. '/',
+            completion = 'file',
+          }
+
+          return (path and path ~= '') and path or dap.ABORT
+        end,
+      },
+      {
+        name = 'Run executable with arguments (GDB)',
+        type = 'gdb',
+        request = 'launch',
+        -- This requires special handling of 'run_last', see
+        -- https://github.com/mfussenegger/nvim-dap/issues/1025#issuecomment-1695852355
+        program = function()
+          local path = vim.fn.input {
+            prompt = 'Path to executable: ',
+            default = vim.fn.getcwd() .. '/',
+            completion = 'file',
+          }
+
+          return (path and path ~= '') and path or dap.ABORT
+        end,
+        args = function()
+          local args_str = vim.fn.input {
+            prompt = 'Arguments: ',
+          }
+          return vim.split(args_str, ' +')
+        end,
+      },
+      {
+        name = 'Attach to process (GDB)',
+        type = 'gdb',
+        request = 'attach',
+        processId = require('dap.utils').pick_process,
+      },
+    }
+
     -- Install golang specific config
     require('dap-go').setup {
       delve = {
