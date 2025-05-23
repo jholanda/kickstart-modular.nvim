6beb8c1f7ff55a7aac27a3648d088b6be5c9c439
João Holanda <joao.holanda@gmail.com>
2025-05-23 16:02:30 -0300

    diminuindo tamanho da borda da janela

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,24 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'folke/snacks.nvim',
+    ---@type snacks.Config
+    opts = {
+      explorer = {
+        -- your explorer configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+      picker = {
+        sources = {
+          explorer = {
+            -- your explorer picker configuration comes here
+            -- or leave it empty to use the default settings
+          },
+        },
+      },
+    },
+  },
+}
