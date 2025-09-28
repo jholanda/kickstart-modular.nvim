12d1c3c5c474e08422d74c811033c2cea359b345
João Holanda <joao.holanda@gmail.com>
2025-09-28 19:56:35 -0300

    adicionado ctrl+s e ctrl+enter

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -42,6 +42,21 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 -- vim.keymap.set("n", "<C-S-j>", "<C-w>J", { desc = "Move window to the lower" })
 -- vim.keymap.set("n", "<C-S-k>", "<C-w>K", { desc = "Move window to the upper" })
 
+--
+-- 1. Ctrl+S para Salvar
+-- Mapeia Ctrl+S para salvar no modo Normal
+vim.keymap.set('n', '<C-s>', ':w<CR>', { desc = 'Salvar arquivo' })
+-- Mapeia Ctrl+S para salvar no modo de Inserção
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', { desc = 'Salvar arquivo' })
+vim.keymap.set('i', '<C-Enter>', '<C-o>:w<CR><CR>', {
+  noremap = true,
+  silent = true,
+  desc = 'Salvar arquivo e inserir nova linha (sem sair do modo de inserção)',
+})
+-- 2. Ctrl+A para Selecionar Tudo
+-- Mapeia Ctrl+A para selecionar todo o texto no modo Normal
+vim.keymap.set('n', '<C-a>', 'ggVG', { desc = 'Selecionar todo o texto' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
